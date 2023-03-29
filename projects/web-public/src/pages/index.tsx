60b9cbf17275a4537cc83932824299d30896d982
Vitor Brancalião <vitor.brancaliao@outlook.com>
2023-03-28 21:02:07 -0300

    Add local example package

--- projects/web-public/src/pages/index.tsx
+++ projects/web-public/src/pages/index.tsx
@@ -1,5 +1,5 @@
-export default function HomePage() {
-  return (
-    <h1>Alura</h1>
-  )
+import { sum } from "@alura/utils/math/sum";
+
+export default function Home() {
+  return <h1> Alura {sum(2, 4)}</h1>;
 }
